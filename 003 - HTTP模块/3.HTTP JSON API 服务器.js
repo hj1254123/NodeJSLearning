984504cd55c4a7eff295d1eb6acf1b68ebb8dd13
HouJian <hj1254123@qq.com>
2020-11-10 23:55:25 +0800

    解决了 JSON API 这个例子的bug
    URL类的第二个参数需要http://example.com这样的完整的基础路径，处理这个BUG浪费了1小时。

--- 003 - HTTP模块/3.HTTP JSON API 服务器.js
+++ 003 - HTTP模块/3.HTTP JSON API 服务器.js
@@ -1,16 +1,21 @@
-/**
-接口一：
-浏览器请求url：/api/parsetime?iso=2017-04-05T12:10:15.474Z
-服务器返回：
- {
-  "hour":21,
-  "minute":45,
-  "second":30
-}
 
-接口二：
-浏览器：/api/unixtime?iso=2017-04-05T12:10:15.474Z
-服务器返回： { "unixtime": 1376136615474 }
+/**
+# JSON API
+
+接口一：根据ISO格式时间返回如下时、分、秒对象。
+
+- 浏览器请求url：/api/parsetime?iso=2017-04-05T12:10:15.474Z
+- 服务器返回：
+    {
+        "hour":21,
+        "minute":45,
+        "second":30
+    }
+
+接口二：根据ISO格式时间返回Unix时间戳
+
+- 浏览器：/api/unixtime?iso=2017-04-05T12:10:15.474Z
+- 服务器返回： { "unixtime": 1376136615474 }
 */
 
 const http = require('http')
@@ -25,18 +30,29 @@ const parsetime = (time) => {
         second: time.getSeconds()
     }
 }
-
+const unixtime = (time) => {
+    return { unixtime: time.getTime() }
+}
 
 const server = http.createServer((req, res) => {
     let { url, headers } = req
-    // let host = headers.host
-    // let myURL = new URL(url, host)
-    // let pathname = myURL.pathname
+    let host = headers.host
+    // 实例化URL，用于操作url
+    let myURL = new URL(url, 'http://' + host)
+    // 用户请求的路径
+    let pathname = myURL.pathname
+    // 实例化Date，用于处理时间
+    let time = new Date(myURL.searchParams.get('iso'))
+    // 待返回的数据
     let result
 
-    if(url === '/') {
+    if(pathname === '/') {
         result = parsetime(new Date())
-    } 
+    } else if(pathname === '/api/parsetime') {
+        result = parsetime(time)
+    } else if(pathname === '/api/unixtime') {
+        result = unixtime(time)
+    }
 
     if(result) {
         res.writeHead(200, { 'Content-Type': 'application/json' })
