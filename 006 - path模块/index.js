edd9e99b304a0b100c781c4c37d3c02defd6a6fc
HouJian <hj1254123@qq.com>
2020-11-25 17:02:45 +0800

    修改了下path，便于做笔记

--- 006 - path模块/index.js
+++ 006 - path模块/index.js
@@ -15,13 +15,11 @@ const path = require('path')
 // 1.basename()
 // 返回路径的basename
 
-const a = path.basename('C:/temp/myfile.html')
-console.log(a) // 'myfile'
+path.basename('C:/temp/myfile.html') // 'myfile'
 
 // 2.parse()
 // 解析路径，返回元信息对象
-const b = path.parse('/home/user/dir/file.txt')
-console.log(b);
+path.parse('/home/user/dir/file.txt')
 /* 
 { root: '/',
   dir: '/home/user/dir',
@@ -32,24 +30,21 @@ console.log(b);
 
 // 3.format()
 // parse方法的反操作，根据元信息对象返回路径字符串
-const c = path.format({
+path.format({
     root: '/',
     dir: '/home/user/dir',
     base: 'file.txt'
 })
-console.log(c);
 // '/home/user/dir/file.txt'
 
 // 4. normalize()
 // 规范化路径
-const d = path.normalize('/foo/bar//baz/asdf/quux/..')
-console.log(d); // '/foo/bar/baz/asdf'
+path.normalize('/foo/bar//baz/asdf/quux/..')// '/foo/bar/baz/asdf'
 
 // 5. join([...paths])
 // 将给定的 path 片段连接到一起（使用平台特定的分隔符作为定界符），然后规范化生成的路径
 // 为了避免不同平台的差异，总是使用这个方法来拼接文件路径
-const e = path.join('/目录1', '目录2', '目录3/目录4', '目录5', '..')
-console.log(e);
+path.join('/目录1', '目录2', '目录3/目录4', '目录5', '..')
 // windows返回：'\目录1\目录2\目录3\目录4'
 // POSIX返回：'/目录1/目录2/目录3/目录4'
 
@@ -70,12 +65,14 @@ path.relative('C:\\orandea\\test\\aaa', 'C:\\orandea\\impl\\bbb');
 // 7.path.resolve([...paths])
 // 将路径或路径片段的序列解析为绝对路径
 // 该方法是从右到左处理的，直到构造出绝对路径
+// 如果到最后还没构建出绝对路径，则使用当前工作目录
 
 path.resolve('/目录1/目录2', './目录3');
 // 返回: '/目录1/目录2/目录3'
 
 path.resolve('/目录1/目录2', '/目录3/目录4/');
-// 已经构造出绝对路径所以返回: '/目录3/目录4'
+// 已经构造出绝对路径所以返回: '/目录3/目录4'。
+// 注意最后的/会被删除，除非是根目录
 
 path.resolve('目录1', '目录2/目录3/', '../目录4/文件.gif');
 // 到最后还没有构造出绝对路径，会根据当前工作区构造
@@ -83,19 +80,21 @@ path.resolve('目录1', '目录2/目录3/', '../目录4/文件.gif');
 // 则返回 '/目录A/目录B/目录1/目录2/目录4/文件.gif'
 
 
+// 8.返回 path 的目录名
+path.dirname('/home/bin/1.txt') // /home/bin
 
-console.log(path.dirname('/home/bin/1.txt')) //返回 path 的目录名
+// 9.返回 path 的扩展名
+path.extname('/home/bin/1.txt') // .txt
 
-console.log(path.extname('/home/bin/1.txt')) //返回 path 的扩展名
-
-console.log(path.sep)// 这个方法返回平台的路径分割符 `/` 或 `\`
+// 10.返回平台的路径分割符 `/` 或 `\`
+path.sep
 // 使用例子
 // windows下
-console.log('foo\\bar\\baz'.split(path.sep))// 返回: ['foo', 'bar', 'baz']
+'foo\\bar\\baz'.split(path.sep)// 返回: ['foo', 'bar', 'baz']
 // POSIX 下
 'foo/bar/baz'.split(path.sep) // 返回: ['foo', 'bar', 'baz']
 
-// path.isAbsolute() 方法检测 path 是否为绝对路径。
+// 11. path.isAbsolute() 方法检测 path 是否为绝对路径。
 // POSIX
 path.isAbsolute('/foo/bar'); // true
 path.isAbsolute('qux/');     // false
